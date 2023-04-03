f8c0cee25ded8a2c77116aaca0855bb01b450a7c
CHANWOO LEE <chw210601@gmail.com>
2023-04-04 00:49:05 +0900

    업비트 웹소켓 연결종료시 재연결 시도하도록 수정

--- src/gateway/web-socket.service.ts
+++ src/gateway/web-socket.service.ts
@@ -3,12 +3,13 @@ import { WebSocket } from 'ws';
 import axios from "axios"
 import { InjectRedis, DEFAULT_REDIS_NAMESPACE } from '@liaoliaots/nestjs-redis';
 import Redis from 'ioredis';
+
 const wsUrl = 'wss://api.upbit.com/websocket/v1';
 
 @Injectable()
 export class WebSocketService {
 
-    private ws: WebSocket = new WebSocket(wsUrl);
+    ws: WebSocket = new WebSocket(wsUrl);
     
     constructor(
         @InjectRedis() private readonly redis: Redis
@@ -31,6 +32,34 @@ export class WebSocketService {
                 console.log("이 메시지는 ",message)
             }
         });
+
+        this.ws.on('close', function() {
+            console.log('Upbit WebSocket 연결 종료');
+            //재연결
+            setTimeout(() => {
+                console.log('Upbit WebSocket 다시 연결 시도중...');
+                const ws = new WebSocket(wsUrl);
+                ws.on('open', async function() {
+                    const market = await axios.get('https://api.upbit.com/v1/market/all')
+                    const marketData = await market.data.map((item:any)=>item.market).filter((item:any)=>item.includes('KRW'))
+                    await redis.set("marketData",marketData.toString())
+                    const request = [{"ticket":"test"},{"type":"trade","codes":marketData}]
+                    ws.send(JSON.stringify(request));
+                    console.log('Upbit WebSocket 연결 성공');
+                });
+                ws.on('message', async function (data : any) {
+                    const message = JSON.parse(data)
+                    if(message){
+                        await redis.set(message.code, data)
+                    }else{
+                        console.log("이 메시지는 ",message)
+                    }
+                });
+                ws.on('close', function() {
+                    console.log('Upbit WebSocket 재연결 실패');
+                });
+            }, 1000);
+        })
     }
 
 
