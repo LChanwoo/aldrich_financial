29d16f619d4b94eadef60911dd9eb790f4713718
CHANWOO LEE <chw210601@gmail.com>
2023-04-29 14:51:11 +0900

    불필요한 코드 삭제

--- src/gateway/event.controller.ts
+++ src/gateway/event.controller.ts
@@ -1,9 +1,5 @@
 
-import { Controller, Get, Inject } from '@nestjs/common';
+import { Controller } from '@nestjs/common';
 
-import { Cache } from 'cache-manager';  
-import { WebSocketService } from './web-socket.service';
-import { RedisService } from '@liaoliaots/nestjs-redis';
-import axios from "axios"
 @Controller()
 export class EventController {}
